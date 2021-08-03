98fc2480decf1b312a2d76363ceb74152d47bacf
suraj gupta <suraj gupta@DESKTOP-DKF37EA>
2021-08-03 17:00:27 +0530

    final project commit digitel 1.0

---
+++ SpringContactApp/src/main/java/in/ezeon/capp/exception/UserBlockedException.java
@@ -0,0 +1,19 @@
+package in.ezeon.capp.exception;
+
+/**
+ *
+ * @author Suraj
+ */
+public class UserBlockedException extends Exception{
+    /**
+     * Creates User object without error description.
+     */
+    public UserBlockedException() {
+    }
+    /**
+     * Creates User object with error description.
+     */
+    public UserBlockedException(String errDesc) {
+        super(errDesc);
+    }    
+}
---
+++ SpringContactApp/src/main/webapp/WEB-INF/view/include/header.jsp
@@ -0,0 +1,1 @@
+<h1><center>Digitel Web - Application</center></h1>
